8ea87cbec0c7dfa95dac0e29e3cbc929fc2a05a5
filippogarbero <garbero.filippo@gmail.com>
2025-10-16 11:05:52 +0200

    stelle

--- sketch.js
+++ sketch.js
@@ -24,7 +24,7 @@ function setup() {
 
   //canvas
   createCanvas(windowWidth,totalHeight);
-  background("lightblue");
+  background("darkblue");
 
   let colCount = 0;
   let rowCount = 0;
@@ -36,30 +36,27 @@ function setup() {
   console.log(data);
 
   let myValue = table.getNum(rowNumber, "column0");
-
   let allValues = table.getColumn("column0");
   let minValue = min(allValues);
   let maxValue = max(allValues);
-  let scaledValue = map(myValue,minValue,maxValue,1,itemSize);
-
-
+  let scaledValue = map(myValue,minValue,maxValue,10,itemSize);
   let value2 = table.getNum(rowNumber, "column2");
-
   let allValues2 = table.getColumn("column2");
   let minValue2 = min(allValues2);
   let maxValue2 = max(allValues2);
   let value2Mapped = map(value2,minValue2,maxValue2,0,1);
 
-  let c1 = color("red");
-  let c2 = color("blue");
+  let c1 = color("yellow");
+  let c2 = color("light yellow");
 
   let mappedColor = lerpColor(c1,c2,value2Mapped); 
 
   let xPos = outerPadding + colCount * (itemSize + padding);
   let yPos = outerPadding + rowCount * (itemSize + padding);
   
-  fill(mappedColor);  
-  rect(xPos,yPos,scaledValue,scaledValue);
+  fill(mappedColor);
+  noStroke();
+  drawStar(xPos + itemSize / 2, yPos + itemSize / 2, scaledValue / 2, scaledValue, 5);
  
 
   colCount++; //aumenta colCount
@@ -74,6 +71,23 @@ function setup() {
 
 }
 
-function draw() {
+
+// funzione per disegnare una stella
+function drawStar(x, y, radius1, radius2, npoints) {
+  let angle = TWO_PI / npoints;
+  let halfAngle = angle / 2.0;
+  beginShape();
+  for (let a = 0; a < TWO_PI; a += angle) {
+    let sx = x + cos(a) * radius2;
+    let sy = y + sin(a) * radius2;
+    vertex(sx, sy);
+    sx = x + cos(a + halfAngle) * radius1;
+    sy = y + sin(a + halfAngle) * radius1;
+    vertex(sx, sy);
+  }
+  endShape(CLOSE);
+}
+
+  function draw() {
   // put drawing code here
 }
